1a533d315f47127a4c1b59cfbd37076a701bea2d
gaobaoyu <gaobaoyu@maoyan.com>
2018-01-29 20:16:50 +0800

    测试

---
+++ src/com/gby/Test.java
@@ -0,0 +1,18 @@
+package com.gby;
+
+/*
+ * Copyright (c) 2018 maoyan.com
+ * All rights reserved.
+ *
+ */
+
+
+/**
+ * 测试
+ *
+ * @author gaobaoyu
+ * @create 2018/01/29
+ **/
+
+public class Test {
+}
